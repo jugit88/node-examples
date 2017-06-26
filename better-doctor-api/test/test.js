6a03c45d9260df530db10405b3c0dbe47af9b00d
Jeremy <jordanudis@yahoo.com>
2017-06-26 17:56:37 -0600

    some cleanup

--- better-doctor-api/test/test.js
+++ better-doctor-api/test/test.js
@@ -1,7 +1,6 @@
 var chai = require('chai')
 var chaiHttp = require('chai-http')
 var server = require('../server')
-// var elasticsearch = require('elasticsearch')
 var requestData = require('../requestData')
 var should = require('chai').should()
 chai.use(chaiHttp)
@@ -27,11 +26,21 @@ describe('BetterDoctor-Proxy', function() {
       .end(function(err, res) {
         res.should.have.status(200)
         res.should.be.json
-        // res.body[0]._source.payload.meta.count.should.equal(55)
         res.body.should.not.have.property('_index')
         res.body.should.not.have.property('_type')
         res.body.should.not.have.property('_id')
-        // res.body[0]._source.payload.data[0].practices[0].location_slug.should.equal('tn-jackson')
+        done()
+      })
+  })
+  it('request ben-harris doctor data from BETTERDOCTOR API', function(done) {
+    chai.request(server)
+      .get('/api/v1/doctors/search?name=ben-harris')
+      .end(function(err, res) {
+        res.should.have.status(200)
+        res.should.be.json
+        res.body.should.not.have.property('_index')
+        res.body.should.not.have.property('_type')
+        res.body.should.not.have.property('_id')
         done()
       })
   })
@@ -49,19 +58,6 @@ describe('BetterDoctor-Proxy', function() {
         done()
       })
   })
-
-  it('request ben-harris doctor data from BETTERDOCTOR API', function(done) {
-    chai.request(server)
-      .get('/api/v1/doctors/search?name=ben-harris')
-      .end(function(err, res) {
-        res.should.have.status(200)
-        res.should.be.json
-        res.body.should.not.have.property('_index')
-        res.body.should.not.have.property('_type')
-        res.body.should.not.have.property('_id')
-        done()
-      })
-  })
   it('add ben-harris doctor data to Database', function(done) {
     chai.request(server)
       .get('/api/v1/doctors/search?name=ben-harris')
@@ -74,19 +70,17 @@ describe('BetterDoctor-Proxy', function() {
         done()
       })
   })
-
-  it('should respond with ALL doctor data from database, which now both', function(done) {
+  it('should respond with ALL doctor data from database, which contains ben-harris and charles-anderson', function(done) {
     chai.request(server)
       .get('/api/v1/doctors/search')
       .end(function(err, res) {
         res.should.have.status(200)
         res.should.be.json
         res.body[0].should.property('_index')
-      //   res.body[0]._index.should.equal('charles-anderson')
         res.body[0].should.property('_type')
         res.body[0].should.have.property('_id')
         res.body[0]._source.payload.meta.item_type.should.equal('Doctor')
-        res.body[0]._source.payload.meta.count.should.equal(25)
+        res.body[0]._source.payload.meta.count.should.equal(55)
         done()
       })
   })
--- better-doctor-api/test/testSetup.sh
+++ better-doctor-api/test/testSetup.sh
@@ -1,2 +1,1 @@
 curl -XDELETE localhost:9200/*
-#curl -XGET localhost:3000/api/v1/doctors/search?name=ben-harris
