041759451e71932ac04ed3695b12f0f3f776b056
Jeremy <jordanudis@yahoo.com>
2017-07-21 13:06:44 -0600

    fixed image sizes to fit the viewer

--- react-starter/src/ImageViewer.js
+++ react-starter/src/ImageViewer.js
@@ -3,17 +3,10 @@ import React from 'react';
 // import Viewer from 'viewerjs';
 // import Lightbox from 'react-images';
 import ImageGallery from 'react-image-gallery';
-import "react-image-gallery/styles/css/image-gallery.css";
+// import "react-image-gallery/styles/css/image-gallery.css";
 
 // var viewer = new Viewer(document.getElementById('images'));
 export default class ImageViewer extends React.Component {
-  constructor () {
-		super();
-		this.state = {
-			lightboxIsOpen: false,
-			currentImage: 0,
-		};
-  }
   buildList() {
     const list = this.props.id.map( (curId,i) =>
       <li key={curId.toString()}>
@@ -26,25 +19,55 @@ export default class ImageViewer extends React.Component {
   builtImageArray() {
     const imagesFormat = this.props.title.map( (curTitle,i) => {
       var Obj = {
-        original: this.props.url[i],
-        thumbnail: this.props.thumbnailUrl[i]
+        original: this.props.thumbnailUrl[i],
+        thumbnail: this.props.thumbnailUrl[i],
+        description: this.props.title[i],
       };
       return Obj;
     });
     return imagesFormat;
   }
   handleImageLoad(event) {
-   console.log('Image loaded', event.target)
+  //  console.log('Image loaded', event.target)
+ }
+  buildTestImages() {
+   const images1 = [
+     {
+       original: 'http://lorempixel.com/1000/600/nature/1/',
+       thumbnail: 'http://lorempixel.com/250/150/nature/1/'
+
+     },
+     {
+       original: 'http://lorempixel.com/1000/600/nature/2/',
+       thumbnail: 'http://lorempixel.com/250/150/nature/2/'
+     },
+     {
+       original: 'http://lorempixel.com/1000/600/nature/3/',
+       thumbnail: 'http://lorempixel.com/250/150/nature/3/'
+     },
+     {
+       original: 'http://i2.wp.com/usatftw.files.wordpress.com/2017/06/usp_mlb-_san_francisco_giants_at_colorado_rockies-2.jpg?crop=0px%2C0px%2C4024px%2C2413px&resize=1000%2C600&ssl=1',
+       thumbnail: 'http://i2.wp.com/usatftw.files.wordpress.com/2017/06/usp_mlb-_san_francisco_giants_at_colorado_rockies-2.jpg?crop=0px%2C0px%2C4024px%2C2413px&resize=1000%2C600&ssl=1'
+     }
+   ]
+  return images1;
  }
   render() {
+    const images = [
+      {
+        original: "http://www.tate.org.uk/art/images/work/D/D09/D09215_8.jpg",
+        thumbnail: "http://www.tate.org.uk/art/images/work/D/D09/D09215_8.jpg"
+      }
+    ]
     return (
       <div>
       <ImageGallery
       items={this.builtImageArray()}
-      slideInterval={2000}
-      onImageLoad={this.handleImageLoad}/>
-
-
+      slideInterval={3000}
+      lazyLoad={true}
+      slideDuration={500}
+      infinite={false}
+      onImageLoad={this.handleImageLoad.bind(this)}/>
       </div>
     );
   }
